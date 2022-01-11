175a9e04872cd4e38a02dee85ed2c6d60b1b1084
opilane <opilane@HP-CZC0359NN8>
2022-01-11 13:48:32 +0200

    fhgdrhdhdgdg

--- EpicBattle/Program.cs
+++ EpicBattle/Program.cs
@@ -1,39 +1,30 @@
 ﻿using System;
-
+using System.IO;
 namespace EpicBattle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] heroes = { "Travis Scotch", "Scooby-Doo", "Sponge Bob", "Patrick" };
-            string[] villians = { "Plankton", "Hitler", "Nigga from Ghetto", "Mall Security", "That guy" };
+            string rootPath = @"C:\Users\opilane\samples\";
+            string[] heroes = GetDataFromFiles(rootPath + "heroes.txt");
+            string[] villians = GetDataFromFiles(rootPath + "villians.txt");
+            string[] weapon = GetDataFromFiles(rootPath + "Weapon.txt");
+            //string[] heroes = { "Travis Scotch", "Scooby-Doo", "Sponge Bob", "Patrick" };
+            //string[] villians = { "Plankton", "Hitler", "Nigga from Ghetto", "Mall Security", "That guy" };
 
-
-            string randomHero = GetRandomCharacter(heroes);
-            string randomvillian = GetRandomCharacter(villians);
-            string heroweapon = GetWeapon();
-            string villiansweapon = GetWeapon();
             Console.WriteLine($"Your random hero is {randomHero}");
+            Console.WriteLine($"Your random hero weapon is {heroweapon}");
             Console.WriteLine($"Your random villian is {randomvillian}");
+            Console.WriteLine($"Your random  villian weapon is {villiansweapon}");
 
         }
 
-        public static string GetRandomCharacter(string[] someArray)
+        public static string[] GetDataFromFiles(string filepath)
         {
-            return someArray[GetRandomIndexForArray(someArray)];
-        }
+            string[] datafromFile = File.ReadAllLines(filepath);
+            return datafromFile;
 
-        public static string GetWeapon()
-        {
-            string[] weapon = { "Spoon", "AWP", "Carabin", "Bible", "Stick" }
-            return weapon[GetRandomIndexForArray(weapon)];
         }
-        public static int GetRandomIndexForArray(string[] someArray)
-        {
-            Random rnd = new Random();
-            return rnd.Next(0, someArray.Length);
-        }
-    
     }
 }
